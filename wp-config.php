fe9ef07eaaf5b6ea6125202abc3d922bb1b73a6b
Marius Ooms <marius@ywam.nl>
2014-09-06 16:03:00 +0200

    Structure commit
    Moving worpress core to wp. Moving wp-content to content. Adding wordpress 4 as a submoule.
    https://github.com/markjaquith/WordPress-Skeleton

--- wp-config.php
+++ wp-config.php
@@ -1,47 +1,15 @@
 <?php
-/**
- * The base configurations of the WordPress.
- *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
- *
- * @package WordPress
- */
+// ===================================================
+// Load database info and local development parameters
+// ===================================================
 
-// Check for a local config file
-if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {
-	include( dirname( __FILE__ ) . '/local-config.php' );
-	define( 'WP_LOCAL_DEV', true );
-	define( 'WP_DEBUG', true );
-} else {
-	define('DB_NAME', 'wordpress_4');
-	define('DB_USER', 'wordpress_9');
-	define('DB_PASSWORD', '#UFz019snA');
-	define('DB_HOST', 'localhost:3306');
-	define( 'WP_LOCAL_DEV', false );	
-	define( 'WP_DEBUG', false );
-}
-
-/**
-* Define type of server
-*
-* Depending on the type other stuff can be configured
-* Note: Define them all, don't skip one if other is already defined
-*/
-
-define( 'DB_CREDENTIALS_PATH', dirname( ABSPATH . '/config/enviroments/' ) ); // cache it for multiple use
+define( 'DB_CREDENTIALS_PATH', dirname( __FILE__ ) . '/config/enviroments/' );
 define( 'WP_LOCAL_SERVER', file_exists( DB_CREDENTIALS_PATH . 'local-config.php' ) );
 define( 'WP_STABLE_SERVER', file_exists( DB_CREDENTIALS_PATH . 'stable-config.php' ) );
 
-/**
-* Load DB credentials
-*/
+// ========================================
+// Select enviroment based on existing file
+// ========================================
 
 if ( WP_LOCAL_SERVER )
     require DB_CREDENTIALS_PATH . 'local-config.php';
@@ -50,23 +18,22 @@ elseif ( WP_STABLE_SERVER )
 else
     require DB_CREDENTIALS_PATH . 'production-config.php';
     
-    
+// ========================
+// Custom Content Directory
+// ========================
+define( 'WP_CONTENT_DIR', dirname( __FILE__ ) . '/content' );
+define( 'WP_CONTENT_URL', 'http://' . $_SERVER['HTTP_HOST'] . '/content' );    
 
-/** Database Charset to use in creating database tables. */
+// ================================================
+// You almost certainly do not want to change these
+// ================================================
 define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
- */
+// ==============================================================
+// Salts, for security
+// Grab these from: https://api.wordpress.org/secret-key/1.1/salt
+// ==============================================================
 if ( ! defined( 'AUTH_KEY' ) )
 	define('AUTH_KEY', 'B$nznrfFYcR08}8}0|k}0,kYc@ckY7quiH6EPEI6+DH6*.x.x+qPjmbA3T7Au6A');
 if ( ! defined( 'SECURE_AUTH_KEY' ) )
@@ -84,33 +51,22 @@ if ( ! defined( 'LOGGED_IN_SALT' ) )
 if ( ! defined( 'NONCE_SALT' ) )
 	define('NONCE_SALT', 'uiLAI:1-_wZht-dlZCKhlOV8:4KS18_[~#;x-elOpxaeHO1ahKS19_[K:5-_ltJ}4');
 
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
+// ==============================================================
+// Table prefix
+// Change this if you have multiple installs in the same database
+// ==============================================================
 $table_prefix  = 'wp_';
 
-/**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
+// ================================
+// Language
+// Leave blank for American English
+// ================================
 define('WPLANG', '');
 
-/**
- * For developers: WordPress debugging mode.
- *
- * Change this to true to enable the display of notices during development.
- * It is strongly recommended that plugin and theme developers use WP_DEBUG
- * in their development environments.
- */
+// ==========================================================
+// Debug mode
+// This conditional will check wether Debug mode should be on
+// ==========================================================
 if ( WP_LOCAL_SERVER || WP_STABLE_SERVER ) {
  
 	define( 'WP_DEBUG', true );
@@ -125,15 +81,14 @@ if ( WP_LOCAL_SERVER || WP_STABLE_SERVER ) {
 	define( 'WP_DEBUG', false );
 }
 
-define('FS_METHOD', 'direct');
-
+// ======================
+// Disable post revisions
+// ======================
 define( 'WP_POST_REVISIONS', false );
 
-/* That's all, stop editing! Happy blogging. */
-
-/** Absolute path to the WordPress directory. */
+// ===================
+// Bootstrap WordPress
+// ===================
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
-
-/** Sets up WordPress vars and included files. */
+	define('ABSPATH', dirname(__FILE__) . '/wp/');
 require_once(ABSPATH . 'wp-settings.php');
